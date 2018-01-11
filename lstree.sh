baef2ea7f4ffd0ae8a91369fd5a71a2fd85194fd
Yokai Seishinkage <nazgul.linux.tutorials@gmail.com>
2018-01-11 04:20:57 -0500

    Added colorization

--- lstree.sh
+++ lstree.sh
@@ -5,6 +5,17 @@
 # This tools is provided to the public in the hopes that it will be useful.
 #
 
+## COLORS ##
+blk=$'\x1b[90m'
+red=$'\x1b[91m'
+grn=$'\x1b[92m'
+ylw=$'\x1b[93m'
+blu=$'\x1b[94m'
+pur=$'\x1b[95m'
+cyn=$'\x1b[96m'
+wht=$'\x1b[97m'
+rst=$'\x1b[0m'
+
 main() {
 
 for f in "${*}"/*
@@ -12,8 +23,8 @@ do
 
     if [ -d "$f" ] ; then
 
-       echo -e "\n┌────[Current Working Directory]Φ[$(pwd)]"
-       echo -e "└──Ͼ>[Listing sub-directory]Φ[${f}]\n"
+       echo -e "\n${ylw}┌────[Current Working Directory]${grn}Φ${ylw}[${red}$(pwd)${ylw}]${rst}"
+       echo -e "${ylw}└──Ͼ>[Listing sub-directory]${red}Φ${ylw}[${grn}${f}${ylw}]${rst}\n"
        main "$f"
 
     elif [ -f "$f" ]; then
